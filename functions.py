c4548cae278918b98227ea5adfba7152b5cbd623
Eunice Tamakloe <euniceteeks@gmail.com>
2018-02-03 07:57:37 -0800

    djangogirls first commit

---
+++ functions.py
@@ -0,0 +1,9 @@
+def say():
+	print('i am a function')
+
+def greeting(name):
+	print('hello'+name)
+
+names=['gerald','rosemary','naa','eunice','anna','geraldine','ethel','doris']
+for name in names:
+	greeting(name)
